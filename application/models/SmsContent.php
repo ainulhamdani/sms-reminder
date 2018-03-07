7c0c9222250d0ecae051b122a728f901a9be2684
AinulHamdani <hamdani.ainul@gmail.com>
2018-03-07 10:06:10 +0800

    fix this dot

--- application/models/SmsContent.php
+++ application/models/SmsContent.php
@@ -4,33 +4,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SmsContent extends CI_Model{
 
-	public $THANKS_MESSAGE = "Selamat pagi bapak/ibu. Terima kasih telah mengentry data Anda. Berikut adalah rekap dari hasil data entry entry Anda kemarin: on-time submission xxx %.";
-	public $REMINDER_MESSAGE = "Selamat sore bapak/ibu. SMS ini merupakan SMS dari sistem. SMS ini sebagai pengingat bapak/ibu untuk mengentry data  dari pelayanan hari ini. Bila bapak/ibu sudah melakukan pengentryan, mohon mensinkronkan aplikasi. Terimakasih.";
-	public $TTD_BIDAN = " Tertanda, Bidan Koordinator";
-	public $TTD_GIZI = " Tertanda, Kepala Puskesmas";
-	public $TTD_VAKSIN = " Tertanda, Koordinator P2P";
+	function __construct() {
+        parent::__construct();
+    }
+
+	private $THANKS_MESSAGE = "Selamat pagi bapak/ibu. Terima kasih telah mengentry data Anda. Berikut adalah rekap dari hasil data entry entry Anda kemarin: on-time submission xxx %.";
+	private $REMINDER_MESSAGE = "Selamat sore bapak/ibu. SMS ini merupakan SMS dari sistem. SMS ini sebagai pengingat bapak/ibu untuk mengentry data  dari pelayanan hari ini. Bila bapak/ibu sudah melakukan pengentryan, mohon mensinkronkan aplikasi. Terimakasih.";
+	private $TTD_BIDAN = " Tertanda, Bidan Koordinator";
+	private $TTD_GIZI = " Tertanda, Kepala Puskesmas";
+	private $TTD_VAKSIN = " Tertanda, Koordinator P2P";
 
 	public function getREMINDER_MESSAGE($fhw){
 		if($fhw=="bidan"){
-			return $REMINDER_MESSAGE.$TTD_BIDAN;
+			return $this->REMINDER_MESSAGE.$this->TTD_BIDAN;
 		}elseif($fhw=="gizi"){
-			return $REMINDER_MESSAGE.$TTD_GIZI;
+			return $this->REMINDER_MESSAGE.$this->TTD_GIZI;
 		}elseif($fhw=="vaksinator"){
-			return $REMINDER_MESSAGE.$TTD_VAKSIN;
+			return $this->REMINDER_MESSAGE.$this->TTD_VAKSIN;
 		}else{
-			return $REMINDER_MESSAGE;
+			return $this->REMINDER_MESSAGE;
 		}
 	}
 
 	public function getTHANKS_MESSAGE($fhw){
 		if($fhw=="bidan"){
-			return $THANKS_MESSAGE.$TTD_BIDAN;
+			return $this->THANKS_MESSAGE.$this->TTD_BIDAN;
 		}elseif($fhw=="gizi"){
-			return $THANKS_MESSAGE.$TTD_GIZI;
+			return $this->THANKS_MESSAGE.$this->TTD_GIZI;
 		}elseif($fhw=="vaksinator"){
-			return $THANKS_MESSAGE.$TTD_VAKSIN;
+			return $this->THANKS_MESSAGE.$this->TTD_VAKSIN;
 		}else{
-			return $THANKS_MESSAGE;
+			return $this->THANKS_MESSAGE;
 		}
 	}
 }
